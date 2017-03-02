bbdd50e0a7b898cf0731f55be77e4ec8e83aeb48
PaulMelnyk <melnyk.paul@googlemail.com>
2017-03-02 18:02:46 +0000

    Update Job.java

--- Job.java
+++ Job.java
@@ -9,21 +9,21 @@ public class Job {
 	
 	public float calculateJob()
 		{
-		return rate*time;
+		return rate*time; //figures out how much the job is worth
 		}
 	
-	public float FinalTotal(float calculateJob,float calculateRoom)
+	public float FinalTotal(float calculateJob,float calculateRoom) //takes in the job total and the total of room
 		{
-		return calculateJob+calculateRoom;
+		return calculateJob+calculateRoom; //adds the values together to get the cost of the job
 		}
 	
 	public float rate()
 		{
-		return rate;
+		return rate; //returns the rate of the job
 		}
 
 	public float time()
 	{
-	return time;
+	return time; //returns the time needed on the job
 	}
 }
