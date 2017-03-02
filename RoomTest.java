fbed6af153e85033beb961144e93d2c48c8ad8b7
PaulMelnyk <melnyk.paul@googlemail.com>
2017-03-02 18:04:03 +0000

    Update RoomTest.java

--- RoomTest.java
+++ RoomTest.java
@@ -5,14 +5,14 @@ import org.junit.Test;
 public class RoomTest {
 
 	@Test
-	public void testCalculateRoom() {
-		int area = 10;
-		int cost = 20;
+	public void testCalculateRoom() { //calculating the cost of the room
+		int area = 10; //stores the area of the room
+		int cost = 20; //stores the cost of the material
 		
-		Room room = new Room(area,cost);
-		int total = Math.round(room.calculateRoom());
+		Room room = new Room(area,cost); //new object room with parameters
+		int total = Math.round(room.calculateRoom()); //total stores the cost of the room
 
-		assertEquals(200,total);
+		assertEquals(200,total); //compares the price of the room to 200, if they dont match then error in the tests
 		
 	}
 
