df9742c931bddd67b7929e24ceb87a4dbc661afe
PaulMelnyk <melnyk.paul@googlemail.com>
2017-03-02 18:01:58 +0000

    Update Room.java

--- Room.java
+++ Room.java
@@ -3,13 +3,13 @@ public class Room {
 	
 	Room(float area,float cost)
 		{
-		this.area = area;
-		this.cost = cost; 
+		this.area = area; //room area
+		this.cost = cost; //cost of the material
 		} 
 	
 	public float calculateRoom()
 		{
-		return area*cost;
+		return area*cost; //returns the cost to furnish the room
 		}
 
 }
