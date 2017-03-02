0daefee7bc283dab25bfdd204be9976eaecdade2
PaulMelnyk <melnyk.paul@googlemail.com>
2017-03-02 18:09:45 +0000

    Update TiledRoom.java

--- TiledRoom.java
+++ TiledRoom.java
@@ -1,20 +1,20 @@
 
 public class TiledRoom extends Room {
-	float grout;
+	float grout; //grout cost for the room
 
 	 public TiledRoom(float grout,float cost,float area){
-		 super(cost,area);
-		 this.grout=area;
+		 super(cost,area); //gets the cost and area for tiled room from its extension (Room)
+		 this.grout=area; //makes grout as equal to area
 		 
 	 }
 	 
-		public float extrarate(float rate)
+		public float extrarate(float rate) //used to figure out the extra rate 
 		{
-		return (float) (rate*1.5);
+		return (float) (rate*1.5); //returns the value of the rate * 1.5
 		}
 		
-		public float tileTotal(float calculateRoom,float extrarate,float time)
+		public float tileTotal(float calculateRoom,float extrarate,float time) //takes in room, rate and time
 		{
-			return (calculateRoom)+time*(extrarate)+grout;
+			return (calculateRoom)+time*(extrarate)+grout; //figures out how much the tiling will cost
 		}
 }
